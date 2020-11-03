bbbfdbbf6c7764ee4f442d63b26b791aa05e8b9b
Zeyong Jin <zeyongj@sfu.ca>
2020-11-03 00:55:07 -0800

    Finish import libraries.

--- app/src/main/java/com/example/cmpt276project/ui/TimeoutTimer.java
+++ app/src/main/java/com/example/cmpt276project/ui/TimeoutTimer.java
@@ -1,8 +1,32 @@
 package com.example.cmpt276project.ui;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 import com.example.cmpt276project.R;
 
