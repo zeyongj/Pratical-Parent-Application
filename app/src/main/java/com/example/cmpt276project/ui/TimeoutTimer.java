f5576b28b865de1d66099559a1f7890573ddf74f
Zeyong Jin <zeyongj@sfu.ca>
2020-11-03 01:04:46 -0800

    Finish implementation.

--- app/src/main/java/com/example/cmpt276project/ui/TimeoutTimer.java
+++ app/src/main/java/com/example/cmpt276project/ui/TimeoutTimer.java
@@ -159,4 +159,233 @@ public class TimeoutTimer extends AppCompatActivity {
             }
         });
     }
+
+    private void setTime(long milliseconds) {
+        mStartTimeInMillis = milliseconds;
+        resetTimer();
+        closeKeyBoard();
+    }
+
+    private void startTimer() {
+        mEndTime = System.currentTimeMillis() + mTimeLeftInMillis;
+
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) { //Every 1 second
+            @Override
+            public void onTick(long l) {
+                mTimeLeftInMillis = l;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                updateWatchInterface();
+            }
+        }.start();
+
+        mTimerRunning = true;
+        updateWatchInterface();
+    }
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        updateWatchInterface();
+    }
+
+
+    private void resetTimer() {
+        if (mTimerRunning) {
+            mCountDownTimer.cancel();
+            mTimerRunning = false;
+        }
+        mTimeLeftInMillis = mStartTimeInMillis;
+        updateCountDownText();
+        updateWatchInterface();
+    }
+
+
+
+    private void updateCountDownText() {
+        int hours = (int) (mTimeLeftInMillis / 1000) / 3600;
+        int minutes = (int) ((mTimeLeftInMillis / 1000) % 3600) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+        String timeLeftFormatted;
+        if (hours > 0) {
+            timeLeftFormatted = String.format(Locale.getDefault(),
+                    "%d:%02d:%02d",hours, minutes, seconds);
+        } else {
+            timeLeftFormatted = String.format(Locale.getDefault(),
+                    "%02d:%02d",minutes,seconds);
+        }
+        mTextViewCountDown.setText(timeLeftFormatted);
+    }
+
+
+    private void updateWatchInterface(){
+        if (mTimerRunning) {
+//            mEditTextInput.setVisibility(View.INVISIBLE);
+//            mButtonSet.setVisibility(View.INVISIBLE);
+//            mButtonSet1Min.setVisibility(View.INVISIBLE);
+//            mButtonSet2Min.setVisibility(View.INVISIBLE);
+//            mButtonSet3Min.setVisibility(View.INVISIBLE);
+//            mButtonSet5Min.setVisibility(View.INVISIBLE);
+//            mButtonSet10Min.setVisibility(View.INVISIBLE);
+//            mButtonReset.setVisibility(View.INVISIBLE);
+            mButtonStartPause.setText("Pause");
+        }
+        else{
+//            mEditTextInput.setVisibility(View.VISIBLE);
+//            mButtonSet.setVisibility(View.VISIBLE);
+//            mButtonSet1Min.setVisibility(View.VISIBLE);
+//            mButtonSet2Min.setVisibility(View.VISIBLE);
+//            mButtonSet3Min.setVisibility(View.VISIBLE);
+//            mButtonSet5Min.setVisibility(View.VISIBLE);
+//            mButtonSet10Min.setVisibility(View.VISIBLE);
+            mButtonStartPause.setText("Start");
+
+            if(mTimeLeftInMillis < 1000) {
+                // Play alarm
+                alarm();
+                // Play vibration
+                vibrate();
+                //Play notification
+                notification();
+                resetTimer();
+
+            }else{
+                mButtonStartPause.setVisibility(View.VISIBLE);
+            }
+
+//            if(mTimeLeftInMillis < mStartTimeInMillis) {
+//                mButtonReset.setVisibility(View.VISIBLE);
+//            } else {
+//                mButtonReset.setVisibility(View.INVISIBLE);
+//            }
+        }
+    }
+
+    private void notification() {
+        // The codes are inspired and modified from the website: https://programmer.group/solutions-to-fail-to-post-notification-on-channel-null.html
+        NotificationManager manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= 26)
+        {
+            //When sdk version is larger than26
+            String id = "channel_1";
+            String description = "143";
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel(id, description, importance);
+            channel.enableLights(true);
+            channel.enableVibration(true);
+            manager.createNotificationChannel(channel);
+            Notification notification = new Notification.Builder(TimeoutTimer.this, id)
+                    .setCategory(Notification.CATEGORY_MESSAGE)
+                    // The icon is downloaded from the website: https://www.flaticon.com/free-icon/notification_1040216?term=notification&page=1&position=16
+                    .setSmallIcon(R.drawable.notification)
+                    .setContentTitle("Alarm")
+                    .setContentText("The timeout timer has reached 0.")
+                    .setAutoCancel(true)
+                    .build();
+            manager.notify(1, notification);
+        }
+        else
+        {
+            //When sdk version is less than26
+            Notification notification = new NotificationCompat.Builder(TimeoutTimer.this)
+                    .setContentTitle("Alarm")
+                    .setContentText("The timeout timer has reached 0.")
+                    .setSmallIcon(R.drawable.notification)
+                    .build();
+            manager.notify(1,notification);
+        }
+    }
+
+    private void vibrate() {
+        // Vibrate for 500 milliseconds
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(500);
+    }
+
+    private void alarm() {
+        Uri alarmSound =
+                RingtoneManager. getDefaultUri (RingtoneManager. TYPE_NOTIFICATION );
+        MediaPlayer mp = MediaPlayer. create (getApplicationContext(), alarmSound);
+        mp.start();
+    }
+
+
+
+//    @Override
+//    protected void onSaveInstanceState(@NonNull Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        outState.putLong("millisLeft", mTimeLeftInMillis);
+//        outState.putBoolean("timerRunning", mTimerRunning);
+//        outState.putLong("endTime", mEndTime);
+//    }
+
+
+//    @Override
+//    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//        mTimeLeftInMillis = savedInstanceState.getLong("millisLeft");
+//        mTimerRunning = savedInstanceState.getBoolean("timerRunning");
+//        updateCountDownText();
+//        updateButtons();
+//        if (mTimerRunning){
+//            mEndTime = savedInstanceState.getLong("endTime");
+//            mTimeLeftInMillis = mEndTime - System.currentTimeMillis();
+//            startTimer();
+//        }
+//    }
+
+    private void closeKeyBoard () {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putLong("startTimeInMillis",mStartTimeInMillis);
+        editor.putLong("millisLeft",mTimeLeftInMillis);
+        editor.putBoolean("timerRunning",mTimerRunning);
+        editor.putLong("endTime",mEndTime);
+
+        editor.apply();
+
+        if (mCountDownTimer != null) {
+            mCountDownTimer.cancel();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        mStartTimeInMillis = prefs.getLong("startTimeInMillis", 600000);
+        mTimeLeftInMillis = prefs.getLong("millisLeft", mStartTimeInMillis);
+        mTimerRunning = prefs.getBoolean("timerRunning",false);
+
+        updateCountDownText();
+        updateWatchInterface();
+
+        if(mTimerRunning) {
+            mEndTime = prefs.getLong("endTime", 0);
+            mTimeLeftInMillis = mEndTime - System.currentTimeMillis();
+            if (mTimeLeftInMillis < 0) {
+                mTimeLeftInMillis = 0;
+                mTimerRunning = false;
+                updateCountDownText();
+                updateWatchInterface();
+            } else {
+                startTimer();
+            }
+        }
+    }
 }
