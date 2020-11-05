3d96ac1ccd465d6a0b8f04f4cbb1c4bab5fbf5b1
wka17 <weixiang_kong@sfu.ca>
2020-11-04 18:57:10 -0800

    Revert "Add head and tail buttons for child to pick with."
    This reverts commit ae934712527009e7e95e6c340b21a0bcb88ed2a8

--- app/src/main/java/com/example/cmpt276project/ui/FlipHistoryActivity.java
+++ app/src/main/java/com/example/cmpt276project/ui/FlipHistoryActivity.java
@@ -2,7 +2,6 @@ package com.example.cmpt276project.ui;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
