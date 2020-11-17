1aba81f3c1bfad45dd7c12f3d69d9acd9ba6822c
Yamato Nakahara <yamrnak@gmail.com>
2020-11-16 21:13:41 -0800

    Add back buttons on HelpPageActivity and WhoseTurnActivity

--- app/src/main/java/com/example/cmpt276project/ui/HelpPageActivity.java
+++ app/src/main/java/com/example/cmpt276project/ui/HelpPageActivity.java
@@ -1,10 +1,14 @@
 package com.example.cmpt276project.ui;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 import com.example.cmpt276project.R;
+
+import java.util.Objects;
 
 public class HelpPageActivity extends AppCompatActivity {
 
@@ -12,5 +16,11 @@ public class HelpPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_page);
+        Toolbar toolbar = findViewById(R.id.helppagetoolbar);
+        setSupportActionBar(toolbar);
+
+        // Enable "up" on toolbar
+        ActionBar ab = getSupportActionBar();
+        Objects.requireNonNull(ab).setDisplayHomeAsUpEnabled(true);
     }
 }
