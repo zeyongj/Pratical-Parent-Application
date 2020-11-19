ec2b7943640d2edf0a69fb36ea052245fd1577b6
xya89 <xiaoyang_yang@sfu.ca>
2020-11-19 12:25:58 -0800

    Upload profile image when adding child

--- app/src/main/java/com/example/cmpt276project/ui/AddChildActivity.java
+++ app/src/main/java/com/example/cmpt276project/ui/AddChildActivity.java
@@ -1,12 +1,18 @@
 package com.example.cmpt276project.ui;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.cmpt276project.R;
 import com.example.cmpt276project.model.Children;
@@ -19,6 +25,12 @@ public class AddChildActivity extends AppCompatActivity {
     private Children children;
     private ChildrenAdapter childrenAdapter;
     private EditText addChildName;
+
+
+
+    // Handling profile Image
+    ImageView profileImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +46,41 @@ public class AddChildActivity extends AppCompatActivity {
         childrenAdapter = ChildrenAdapter.getInstance();
         addChildName = findViewById(R.id.txt_enterChildName);
 
+        profileImage = findViewById(R.id.profileImage);
+
 
 
         registerClickedSaveChild();
         registerClickedCancel();
 
+        registerClickedChangeProfile();
 
 
+
+    }
+
+    private void registerClickedChangeProfile() {
+        Button btn = findViewById(R.id.btn_changeProfileImage);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // open gallery
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGalleryIntent, 1000);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) {
+            if(resultCode == Activity.RESULT_OK) {
+                Uri imageUri = data.getData();
+                profileImage.setImageURI(imageUri);
+            }
+        }
     }
 
     // TODO: fix bug when saving child with no names
