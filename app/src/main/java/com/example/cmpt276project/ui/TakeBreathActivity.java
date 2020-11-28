e143faef0993839bc2cb524fab5332bf36debe53
xya89 <xiaoyang_yang@sfu.ca>
2020-11-27 22:14:26 -0800

    Create TakeBreathActivity. Make the Main Menu buttons go to TakeBreathActivity

---
+++ app/src/main/java/com/example/cmpt276project/ui/TakeBreathActivity.java
@@ -0,0 +1,24 @@
+package com.example.cmpt276project.ui;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cmpt276project.R;
+
+import java.util.Objects;
+
+public class TakeBreathActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_breath);
+
+
+
+
+
+    }
+}
