aa5f0ff3043fc09c91e085bf59db815ecc34a4b2
xya89 <xiaoyang_yang@sfu.ca>
2020-11-28 18:28:23 -0800

    Saved number of breaths selected to sharedPreference

--- app/src/main/java/com/example/cmpt276project/ui/TakeBreathActivity.java
+++ app/src/main/java/com/example/cmpt276project/ui/TakeBreathActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +15,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.example.cmpt276project.R;
@@ -19,6 +23,9 @@ import com.example.cmpt276project.R;
 import java.util.Objects;
 
 public class TakeBreathActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    public static final String PREF_SPINNER = "Number of breath spinner sharedPreference";
+    public static final String NUMBER_OF_BREATH_SELECTED = "Number of breath selected";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,8 +112,22 @@ public class TakeBreathActivity extends AppCompatActivity implements AdapterView
         ((TextView)spinner.getChildAt(0)).setTextSize(16);
 
         String text = parent.getItemAtPosition(position).toString();
-        // TODO: Save number of breaths into SharedPreference
+        saveNumBreath(Integer.parseInt(text));
 
+        // Test if the value is saved
+        //Toast.makeText(this, "The number saved is:" + getNumBreath(this), Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void saveNumBreath(int numBreath) {
+        SharedPreferences prefs = this.getSharedPreferences(PREF_SPINNER, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(NUMBER_OF_BREATH_SELECTED, numBreath);
+        editor.apply();
+    }
+    static public int getNumBreath(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(PREF_SPINNER, MODE_PRIVATE);
+        return prefs.getInt(NUMBER_OF_BREATH_SELECTED, 1);
     }
 
     @Override
