a3c19df755b7b1335edee8a6c50d84007bed5a5c
xya89 <xiaoyang_yang@sfu.ca>
2020-11-29 17:13:04 -0800

    Add button hold feature in BreathingActivity

--- app/src/main/java/com/example/cmpt276project/ui/BreathingActivity.java
+++ app/src/main/java/com/example/cmpt276project/ui/BreathingActivity.java
@@ -3,7 +3,9 @@ package com.example.cmpt276project.ui;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -11,6 +13,19 @@ import android.widget.Toast;
 import com.example.cmpt276project.R;
 
 import java.util.Objects;
+
+/* Messages worth noticing:
+* 1. When first entering the activity, Exhale button should be invisible. Exhale button should only be
+*   visible when one inhale process is finished.
+* 2. The inhale and exhale processes should take place in the same activity.
+* 3. Inhale button onClick should display a help message, on hold should start the inhale process.
+* 4. *** Currently i'm not sure about what to do with the exhale button. ***
+*
+* (this is only my opinion on the ui mechanism, we may
+* discuss about how the ui should behave later)
+* */
+
+
 
 public class BreathingActivity extends AppCompatActivity {
 
@@ -25,18 +40,80 @@ public class BreathingActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         Objects.requireNonNull(ab).setDisplayHomeAsUpEnabled(true);
 
+
+        Button exhaleButton = findViewById(R.id.btn_exhale);
+        setButtonVisible(exhaleButton, View.INVISIBLE);
+
         registerClickedStart();
 
 
     }
 
+    private void setButtonVisible(Button button, int visibility) {
+        button.setVisibility(visibility);
+    }
+
     private void registerClickedStart() {
         Button btn = findViewById(R.id.btn_inhale);
         btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Toast.makeText(BreathingActivity.this, BREATH_IN_BUTTON_HELP, Toast.LENGTH_SHORT).show();
+           }
+       });
+
+
+        // Should use LongClick Listener for "hold"
+        // TODO: fix bug when help message displayed when releasing the button.
+        btn.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(BreathingActivity.this, BREATH_IN_BUTTON_HELP, Toast.LENGTH_SHORT).show();
+            public boolean onLongClick(View v) {
+                Toast.makeText(BreathingActivity.this, "bruh", Toast.LENGTH_SHORT).show();
+                return false;
             }
         });
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
